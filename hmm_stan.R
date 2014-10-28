77b2a6ddf3585d3cf6a5ddf17a1a71bd22be1092
facewhite <leechanhee@LeeChanheeui-MacBook-Air.local>
2014-10-28 21:34:19 +0900

    added little

--- hmm_stan.R
+++ hmm_stan.R
@@ -1,16 +1,19 @@
 library("data.table")
 library("rstan")
 
+maxTravel <- 29
+
+
 code_region <- function(regionname) {
     regionlist <- list("A","C","E","F","H","J","P","S")
     return(grep(regionname, regionlist))
 }
 
-table_to_matrix <- function(table, fcol, scol) {
+table_to_matrix <- function(table) {
     
 }
 
-t1 <- data.table(read.csv("over10.csv")) # 40943 records
+t1 <- data.table(read.csv("~/Documents/hanatour/hanatour/over10.csv")) # 40943 records
 t1$booking_type <- factor(t1$booking_type)
 t1$package_code <- factor(t1$package_code)
 t1$area_code <- factor(t1$area_code)
@@ -21,12 +24,15 @@ t1$booking_path <- factor(t1$booking_path)
 t1$cust_no <- factor(t1$cust_no)
 t1$seq <- factor(t1$seq)
 
-t2 <- t1[total_n<30] # 39994 records
+t2 <- t1[total_n<=maxTravel] # 39994 records
 
 inds <- unique(t2$cust_no)
 
+region_matrix <- matrix()
 for (i in inds){
-    records <- t2[]
+    records <- t2[cust_no == i]
+    records <- records[with(records, order(nth))]
+
 }
 
 rs <- sapply(t2$regions,code_region)
