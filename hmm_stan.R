c01f991f190a6abdd08511f627cdeebe2cf3584a
facewhite <leechanhee@LeeChanheeui-MacBook-Air.local>
2014-11-02 16:50:06 +0900

    change sampling methods
    there is some customers with large n and small number of package travel

--- hmm_stan.R
+++ hmm_stan.R
@@ -14,39 +14,42 @@ table_to_matrix <- function(table) {
 }
 
 # MAC
-#t1 <- data.table(read.csv("~/Documents/hanatour/hanatour/over10.csv")) # 40943 records
+t1 <- data.table(read.csv("~/Documents/hanatour/hanatour/over10.csv")) # 40943 records
 
 # Windows
-t1 <- data.table(read.csv("over10.csv")) # 40943 records
+#t1 <- data.table(read.csv("over10.csv")) # 40943 records
+t1Tab <- table(t1$cust_no)
+t2 <- t1[cust_no %in% names(t1Tab[t1Tab>10])]
 
-t2 <- t1[total_n <= maxTravel] # 39994 records
-tt <- t2[cust_no %in% sample(unique(t2$cust_no), 1000)] # sample 1000 customers
 
-tt$booking_type <- factor(tt$booking_type)
-tt$package_code <- factor(tt$package_code)
-tt$area_code <- factor(tt$area_code)
-#tt$accos <- factor(tt$accos)
-tt$province <- factor(tt$province)
-#tt$gender <- factor(tt$gender)
-tt$booking_path <- factor(tt$booking_path)
-tt$cust_no <- factor(tt$cust_no)
-tt$seq <- factor(tt$seq)
+#t2 <- t1[total_n <= maxTravel] # 39994 records
+#tt <- t2[cust_no %in% sample(unique(t2$cust_no), 1000)] # sample 1000 customers
 
-tt$regions <- sapply(tt$regions,code_region)
+t2$booking_type <- factor(t2$booking_type)
+t2$package_code <- factor(t2$package_code)
+t2$area_code <- factor(t2$area_code)
+#t2$accos <- factor(t2$accos)
+t2$province <- factor(t2$province)
+#t2$gender <- factor(t2$gender)
+t2$booking_path <- factor(t2$booking_path)
+t2$cust_no <- factor(t2$cust_no)
+t2$seq <- factor(t2$seq)
+
+t2$regions <- sapply(t2$regions,code_region)
 
 # for (i in inds){
-#     records <- tt[cust_no == i]
+#     records <- t2[cust_no == i]
 #     records <- records[with(records, order(cust_no, nth))]
 # }
 
-tt <- tt[with(tt, order(cust_no, nth))]
+t2 <- t2[with(t2, order(cust_no, nth))]
 custbuf <- 0
 regionbuf <- vector()
 accosbuf <- vector()
 genders <- vector()
 
-for (rownum in 1:nrow(tt)) {
-    row <- tt[rownum,]
+for (rownum in 1:nrow(t2)) {
+    row <- t2[rownum,]
     cno <- row$cust_no
     region <- row$regions
     accos <- row$accos
@@ -68,7 +71,7 @@ regionbuf <- c(regionbuf, rep(0,maxTravel - (length(regionbuf) %% maxTravel)))
 region_matrix <- matrix(regionbuf, ncol = maxTravel, byrow=TRUE)
 
 accosbuf <- tail(accosbuf, -1 * maxTravel)
-accosbuf <- c(accosbuf, rep(0,maxTravel  -(length(accosbuf) %% maxTravel)))
+accosbuf <- c(accosbuf, rep(0, maxTravel  -(length(accosbuf) %% maxTravel)))
 accos_matrix <- matrix(accosbuf, ncol = maxTravel, byrow=TRUE)
 
 genders <- c(tail(genders,-1), row$gender)
@@ -83,5 +86,4 @@ stan_table <- list (I = length(unique(t2$cust_no)),
 
 model_ver1 <- '
 model {
-    
 }'
